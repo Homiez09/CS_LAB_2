8bffa75effb7303e6ff21b080e3200d3a8e0afd3
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2022-12-10 15:18:17 +0700

    LAB02

--- LAB02/04.c
+++ LAB02/04.c
@@ -2,17 +2,16 @@
 #include <stdlib.h>
 
 void main() {
-    double salary, tax;
+    float salary, tax; // double is 8 bytes, float is 4 bytes
     char salary_str[20];
 
     fgets(salary_str, 20, stdin);
     salary = atof(salary_str);
 
     if (salary >= 0) {
-        if (salary >= 300000) { // > 300k
-            tax += 15000;
-            salary -= 300000;
-            tax += salary * 0.1;
+        if (salary >= 300000.0) { // > 300k
+            salary -= 300000.0;
+            tax = 15000.0 + (salary * 0.1);
         } else { // < 100k
             tax = salary * 0.05;
         }
